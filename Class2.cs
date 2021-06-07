85286ec6e4b3e23a19d887e45397b851eb799b23
Dedmix <84181853+Dedmix@users.noreply.github.com>
2021-06-07 16:38:27 +0500

    Add files via upload

---
+++ Class2.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp19
+{
+	class MyList<T>
+
+	{
+		public void Add(T value)
+		{
+			Node<T> asd = new Node<T>();
+			asd.value = value;
+			if (firstElement == null)
+			{
+				firstElement = asd;
+			}
+			else
+			{
+				GetLast().next = asd;
+			}
+
+
+		}
+		Node<T> firstElement;
+
+		Node<T> GetLast()
+		{
+			Node<T> last = firstElement;
+
+			while (last.next != null)
+			{
+				last = last.next;
+			}
+			return last;
+		}
+		public void Print()
+		{
+			Node<T> last = GetFirst();
+			while (last != null)
+			{
+				Console.WriteLine(last.value);
+				last = last.next;
+
+			}
+		}
+		public void AddFront(T value) 
+		{
+			Node<T> abc = new Node<T>();
+			abc.value = value;
+
+			if (firstElement == null)
+			{
+				firstElement = abc;
+			}
+			else
+			{
+				abc.next = GetFirst();
+				abc.next.prev = abc;
+			}
+		}
+		Node<T> GetFirst()
+
+		{
+			Node<T> last = firstElement;
+
+			while (last.prev != null)
+			{
+				last = last.prev;
+			}
+			return last;
+		}
+
+
+
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp19
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			MyList<string> list = new MyList<string>();
+			list.Add("Безумие это точное повторение одного и того же действия раз за разом,");
+			list.Add("В надежде на изменнение это и безумие ");
+			list.Add("Когда впервые я это услышал, не помню кто сказал эту хрень я БУМ, убил его");
+			list.Add("Смысл в том хахаха Окей? Он был прав.");
+			list.AddFront("Я уже говорил что такое безумиею А?");
+			list.Print();
+
+
+
+		}
+	}
+}
